a1896832483864378ccca61905003d72848fc67f
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-03-26 10:13:16 +0530

    Create Additive sequence.py

---
+++ Additive sequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAdditiveSequence(self, s):
+        def check(n1, n2, s):
+            if not s:
+                return True
+            n = n1 + n2
+            ns = str(n)
+            if not s.startswith(ns):
+                return False
+            return check(n2, n, s[len(ns):])
+        n = len(s)
+        for i in range(1, n):
+            for j in range(i + 1, n):
+                n1, n2 = int(s[:i]), int(s[i:j])
+                if check(n1, n2, s[j:]):
+                    return 1
+        return 0
