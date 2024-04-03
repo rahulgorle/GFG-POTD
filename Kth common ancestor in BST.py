acd4c0ba7be507e0ea6b767f1fd36ec4a7c13911
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-04-03 08:09:18 +0530

    Create Kth common ancestor in BST.py

---
+++ Kth common ancestor in BST.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kthCommonAncestor(self, root, k, x, y):
+        # Make sure x < y
+        if x > y:
+            x, y = y, x
+        ancestors, node = [root.data], root
+        while not (x <= node.data <= y):
+            if x < node.data:
+                node = node.left
+            else:
+                node = node.right
+            ancestors.append(node.data)
+        return ancestors[-k] if len(ancestors) >= k else -1
