292793d686761069a16783b59e76b4edb8e6f760
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-04-22 10:37:16 +0530

    Create Row with minimum number of 1's.py

---
+++ Row with minimum number of 1's.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minRow(self,n,m,a):
+        ans = {}
+        for i in range(n):
+            ans[i+1] = a[i].count(1)
+        return sorted(ans.items(), key = lambda t:t[1])[0][0]
