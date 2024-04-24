d32363b1e26d910bfdccf4014f3d1ada70005a24
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-04-24 10:42:07 +0530

    Create Paths to reach origin.py

---
+++ Paths to reach origin.py
@@ -0,0 +1,12 @@
+class Solution:
+    def ways(self, n, m):
+        mod = 10 ** 9 + 7
+        x = min(n, m)
+        num = n + m
+        p = 1
+        q = 1
+        for i in range(1, x + 1):
+            p *= num
+            num -= 1
+            q *= i
+        return (p // q) % mod
