309070208a236cd1e6662ed10402ba25ea9af5f5
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-05-10 11:50:53 +0530

    Create Combination Sum II.py

---
+++ Combination Sum II.py
@@ -0,0 +1,22 @@
+class Solution:
+    def CombinationSum2(self, arr, n, k):
+        ans = []
+        arr.sort()
+        def fun(idx, comb, s):
+            if idx == n or s > k:
+                if s == k:
+                    ans.append(comb)
+                return
+            
+            if s == k:
+                ans.append(comb)
+                return
+            
+            for i in range(idx, n):
+                if i > idx and arr[i - 1] == arr[i]:
+                    continue
+                s += arr[i]
+                fun(i + 1, comb + [arr[i]], s)
+                s -= arr[i]
+        fun(0, [], 0)
+        return ans
