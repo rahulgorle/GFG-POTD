9fe039f0408c344c6536d3675ca18ca2f0102fbf
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-06-09 08:13:04 +0530

    Create Convert array into Zig-Zag fashion.py

---
+++ Convert array into Zig-Zag fashion.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def zigZag(self, n : int, arr : List[int]) -> None:
+        n = len(arr)
+        for i in range(n - 1):
+            if i % 2 == 0:
+                if arr[i] > arr[i + 1]:
+                    arr[i], arr[i + 1] = arr[i + 1], arr[i]
+            else:
+                if arr[i] < arr[i + 1]:
+                    arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        return arr
