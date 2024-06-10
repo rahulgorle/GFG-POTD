51675745cfe3ebec4bc848ad02dbd7964fa024f4
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-06-10 11:29:38 +0530

    Create Nuts and Bolts Problem.py

---
+++ Nuts and Bolts Problem.py
@@ -0,0 +1,10 @@
+class Solution:
+ 
+   def matchPairs(self, n, nuts, bolts):
+        order = [ '!','#','$','%','&','*','?','@','^']
+        n = []
+        for i in order:
+            if i in nuts and i in bolts:
+                n.append(i)
+        nuts[:] = n
+        bolts[:] = n
