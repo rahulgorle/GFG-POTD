2621a139f966edc6c1e43eb26cf4ca3a30192aa0
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-06-12 09:07:46 +0530

    Create Count numbers containing 4.py

---
+++ Count numbers containing 4.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countNumberswith4(self, n : int) -> int:
+        res = 0
+        for i in range(1, n + 1):
+            if "4" in str(i):
+                res += 1
+        return res
