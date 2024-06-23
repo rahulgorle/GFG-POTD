e430879bf7baf0046985adbfe09ec42e28d1f015
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-06-23 10:40:41 +0530

    Create Print Bracket Number.py

---
+++ Print Bracket Number.py
@@ -0,0 +1,14 @@
+class Solution:
+	def bracketNumbers(self, str):
+	    stack = [] 
+	    c = 0
+	    ans = []
+	    for i in range(len(str)):
+	        if(str[i] == '('):
+	            c += 1 
+	            ans.append(c)
+	            stack.append(c)
+	        elif(str[i] == ')'):
+	            ans.append(stack[-1]) 
+	            stack.pop() 
+	    return ans
