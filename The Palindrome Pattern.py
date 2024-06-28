23fe8120a82c35d48ebf7d52a2b023075fdcd852
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-06-28 08:47:12 +0530

    Create The Palindrome Pattern.py

---
+++ The Palindrome Pattern.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pattern (self, arr):
+        n = len(arr)
+        m = len(arr[0]) if n > 0 else 0
+    
+        for i in range(n):
+            if arr[i] == arr[i][::-1]:
+                return str(i) + ' ' + "R"
+        for j in range(m):
+            col = [arr[i][j] for i in range(n)]
+            if col == col[::-1]:
+                return str(j)+ ' ' +"C"
+        return -1
