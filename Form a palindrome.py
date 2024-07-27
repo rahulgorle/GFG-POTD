f72da66a641e1c78ba62a409c9f1c6de75a5922e
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-07-27 14:46:43 +0530

    Create Form a palindrome.py

---
+++ Form a palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countMin(self, s):
+        dp = [[0] * len(s) for _ in range(len(s))]
+        maxLen = 0
+        for i in reversed(range(len(s))):
+            dp[i][i] = 1
+            for j in range(i + 1, len(s)):
+                dp[i][j] = max(dp[i][j - 1], dp[i + 1][j])
+                if s[i] == s[j]:
+                    dp[i][j] = max(dp[i][j], dp[i + 1][j - 1] + 2)
+                maxLen = max(dp[i][j], maxLen)
+    
+        return len(s) - maxLen
