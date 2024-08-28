4fb4d280c35a5558d4a28beeead88385d9f776ce
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-08-28 20:09:45 +0530

    Create Sorting Elements of an Array by Frequency.py

---
+++ Sorting Elements of an Array by Frequency.py
@@ -0,0 +1,6 @@
+from collections import Counter 
+class Solution:
+    def sortByFreq(self,arr):
+        freq = Counter(arr)
+        s = sorted(arr, key = lambda x:(-freq[x], x))
+        return s
