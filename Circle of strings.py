9ed262a74f341b0f3eb0b370debbaa5342bd79df
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-09-10 23:13:43 +0530

    Create Circle of strings.py

---
+++ Circle of strings.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isCircle(self, arr):
+        from collections import defaultdict, Counter
+        g = defaultdict(list)
+        indegree = Counter()
+        outdegree = Counter()
+        for s in arr:
+            g[s[0]].append(s[-1])
+            indegree[s[-1]] += 1
+            outdegree[s[0]] += 1
+        
+        if indegree != outdegree:
+            return 0
+        
+        visited = set()
+        def dfs(n, g):
+            if n in visited:
+                return
+            visited.add(n)
+            for nbr in g[n]:
+                dfs(nbr, g)
+        
+        dfs(arr[0][0], g)
+        if len(visited) != len(g):
+            return 0
+        return 1
