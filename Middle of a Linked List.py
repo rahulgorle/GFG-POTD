1f8578dc0263e08d38f88036b96d1a3acf999ad5
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-09-12 20:15:15 +0530

    Create Middle of a Linked List.py

---
+++ Middle of a Linked List.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getMiddle(self, head):
+        l1 = []
+        while head:
+            l1.append(head.data)
+            head = head.next
+        return l1[len(l1) // 2]
