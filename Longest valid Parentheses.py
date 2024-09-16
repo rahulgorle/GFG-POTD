b7f587dd9410aca469b8f22cf01f34dfabf1a4c3
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-09-16 23:58:28 +0530

    Create Longest valid Parentheses.py

---
+++ Longest valid Parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxLength(self, s):
+        stack = [-1]
+        max_len = 0
+        
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if stack:
+                    max_len = max(max_len, i - stack[-1])
+                else:
+                    stack.append(i)
+        
+        return max_len
