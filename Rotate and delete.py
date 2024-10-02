83c5b0e2e874001481305291428c4fe8e7e6cfee
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-10-02 11:50:22 +0530

    Create Rotate and delete.py

---
+++ Rotate and delete.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rotateDelete(self,  arr):
+        n = len(arr)
+        for k in range(1, n // 2 + 1):
+            arr.insert(0, arr.pop())
+            arr.pop(-k)
+        return arr[0]
