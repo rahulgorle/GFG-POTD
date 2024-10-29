db4e0ab0f8feab4c9f95ab7d365e195785d03f31
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-10-29 22:04:33 +0530

    Create Quick Sort on Linked List.py

---
+++ Quick Sort on Linked List.py
@@ -0,0 +1,26 @@
+def quickSort(head):
+    
+    def sort(h, t):
+        if not h or h == t or h.next == t:
+            return h
+            
+        v = h.data
+        dummy = Node(0)
+        dummy.next = h
+        
+        prev, w = dummy, h
+        
+        while w != t:
+            nw = w.next
+            if w.data < v:
+                prev.next = w.next
+                w.next = dummy.next
+                dummy.next = w
+            else:
+                prev = prev.next
+                
+            w = nw
+        
+        h.next = sort(h.next, t)
+        return sort(dummy.next, h)
+    return sort(head, None)
