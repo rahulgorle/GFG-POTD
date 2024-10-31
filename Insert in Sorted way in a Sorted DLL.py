6b9a56f542322a76cd890d7d24e057f4e1dc3fc3
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-10-31 17:57:55 +0530

    Create Insert in Sorted way in a Sorted DLL.py

---
+++ Insert in Sorted way in a Sorted DLL.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortedInsert(self, head, x):    
+        new_node = Node(x)
+        if head is None or head.data >= x:
+            new_node.next = head
+            if head:
+                head.prev = new_node
+            return new_node          
+        current = head
+        while current.next and current.next.data < x:
+            current = current.next
+
+        new_node.next = current.next
+        if current.next:
+            current.next.prev = new_node
+        new_node.prev = current
+        current.next = new_node
+
+        return head
