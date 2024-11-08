cbc5d8941a2bee0d32267d156c26f145655b3439
rahulgorle <120768411+rahulgorle@users.noreply.github.com>
2024-11-08 23:42:08 +0530

    Create Minimum repeat to make substring.py

---
+++ Minimum repeat to make substring.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minRepeats(self, s1, s2):
+        n1 = len(s1)
+        n2 = len(s2)
+        k = (n2 // n1) + n1
+        s = s1
+        count = 1
+        i = 0
+        while i < k:
+            if s.find(s2) != -1:
+                return count
+            else:
+                count += 1
+                s += s1
+            i += 1
+        return -1
